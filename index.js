741252d5e76e57319aabd4862126239865104184
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-23 05:05:53 +0900

    implement 'list'

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 
-module.exports = function () {
+var parse = require('css-parse');
+
+module.exports.list = function (css) {
+  var ast = parse(css);
+  var stackingRuleName = [];
+
+  ast.stylesheet.rules.forEach(function visit (rule) {
+    if (rule.rules) rule.rules.forEach(visit);
+
+    if (hasStackingContext (rule)) stackingRuleName.push(hasStackingContext);
+  });
+
+  return StackingRuleName;
 };
+
+function has (rule, property) {
+  rule.declarations.forEach(function (declaration) {
+    if (declaration.property === property) return true;
+    else return false;
+  });
+}
+
+function hasStackingContext (rule) {
+  var z_val, position_val;
+  rule.declarations.forEach(function (declaration) {
+    if (declaration.property === 'z-index') z_val = declaration.value;
+    if (declaration.property === 'position') position_val = declaration.value;
+  });
+
+  if ((position_val === 'fixed' || position_val === 'sticky')
+  && ((position_val === 'relative' || position_val === 'absolute') && z_val !=== 'auto' )) {
+    return rule.selectors;
+  }
+  else return null;
+}
